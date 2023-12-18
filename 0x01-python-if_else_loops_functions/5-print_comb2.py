1e32a8858a996bc84db95134848917e79d830f2b
lami_lekan <olagbajuisrael11@gmail.com>
2023-12-18 22:48:17 +0000

    Loops

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print('0{}'.format(i), end=', ')
-    elif i == 99:
+    if i == 99:
         print(i)
     else:
-        print(i, end=', ')
+        print('{}'.format('0' + str(i) if i < 10 else i), end=', ')
