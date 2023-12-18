ec1bb3a096e6982404b2a2f107ec9c054f8e4cc2
lami_lekan <olagbajuisrael11@gmail.com>
2023-12-18 21:43:35 +0000

    Loops

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        print('{}{}'.format(i,j,), end=', ')
+for i in range(100):
+    if i < 10:
+        print('0{}'.format(i), end=', ')
+    else:
+        print(i, end=', ')
