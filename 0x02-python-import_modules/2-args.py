5439b6996679d5cedb2ced6043a64f92807313b3
lami_lekan <olagbajuisrael11@gmail.com>
2024-01-06 14:45:01 +0000

    Using argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 2:
+        print("{} argument:".format(len(argv)-1))
+        print("{}: {}".format(len(argv)-1, argv[1]))
+    else:
+        if len(argv) == 1:
+            print("{} arguments.".format(len(argv)-1))
+        else:
+            print("{} arguments:".format(len(argv)-1))
+        for i in range(len(argv)+1):
+            print("{}: {}".format(i+1, argv[i+1]))
