42bbc61d34606fd96be4582617214571e7f770fe
lami_lekan <olagbajuisrael11@gmail.com>
2024-01-13 19:31:26 +0000

    Index retrival

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,5 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    else:
-        return (my_list[idx])
+    elif idx > len(my_list):
+        return None
+    return (my_list[idx])
