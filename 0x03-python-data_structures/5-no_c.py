1c80b2896e9c4e13ec7339a60f23af0e4621811f
lami_lekan <olagbajuisrael11@gmail.com>
2024-01-22 01:03:29 +0000

    Slice deletion-1

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-
+    new_string = ""
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
-            new_string = my_string[:i] + my_string[i+1:]
-            return new_string
+            continue
+        new_string += my_string[i]
+    return new_string
