c8e929e7a77b78e78acac7016314ae5976c6f410
lami_lekan <olagbajuisrael11@gmail.com>
2024-01-22 00:36:11 +0000

    Slice deleteWq

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+
+    for i in range(len(my_string)-1):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            new_string = my_string[:i] + my_string[i+1:]
+            return new_string
