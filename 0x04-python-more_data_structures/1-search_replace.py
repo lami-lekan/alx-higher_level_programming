0683e03ecbd01de1bdcb71698e5debd4945d7e2c
lami_lekan <olagbajuisrael11@gmail.com>
2024-02-17 21:54:06 +0000

    search & replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    if len(my_list) == 0:
+        return new_list
+
+    for i in range(len(my_list)):
+        new_list.append(my_list[i])
+
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
