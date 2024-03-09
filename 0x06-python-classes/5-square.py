c1d08dceba98c3e69340c4641b0eb85e6813b1ef
lami_lekan <olagbajuisrael11@gmail.com>
2024-03-09 01:30:05 +0000

    Print method

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''A square class'''
+
+
+class Square:
+
+    '''Initilization of field'''
+    def __init__(self, size=0):
+        self.__size = size
+
+    '''getter'''
+    @property
+    def size(self):
+        return self.__size
+
+    '''setter'''
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    '''Area method'''
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print('#' * self.__size)
